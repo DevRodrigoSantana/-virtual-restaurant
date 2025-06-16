cc663c9b23721b2de9a5cf27788346239739a247
DevRodrigoSantana <dev.rodrigosantana@hotmail.com>
2025-06-15 22:28:49 -0300

    feat(config): Inicializa projeto Spring Boot com dependencias

---
+++ src/main/java/com/restaurantevirtual/RestauranteVirtualApplication.java
@@ -0,0 +1,13 @@
+package com.restaurantevirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestauranteVirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestauranteVirtualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restaurantevirtual/RestauranteVirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurantevirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestauranteVirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
